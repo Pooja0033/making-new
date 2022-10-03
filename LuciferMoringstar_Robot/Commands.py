8d6de39cfca2462e411072ba907b9c0a74092a1a
Pooja0033 <113719724+Pooja0033@users.noreply.github.com>
2022-10-03 11:29:02 +0530

    Update Commands.py

--- LuciferMoringstar_Robot/Commands.py
+++ LuciferMoringstar_Robot/Commands.py
@@ -84,7 +84,7 @@ async def start(bot, message):
                     f_caption = f"{files.file_name}"
                 buttons = [
                     [
-                        InlineKeyboardButton('🎖 DEPLOY YOURS 🎖', url=f'{TUTORIAL}')
+                        InlineKeyboardButton('☣️ 𝙅𝙊𝙄𝙉 ☣️', url=f'{TUTORIAL}')
                     ]
                     ]
                 await bot.send_cached_media(
