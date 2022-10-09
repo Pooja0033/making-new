309084fac950566907c928717d3b7ce35c9c8589
Pooja0033 <113719724+Pooja0033@users.noreply.github.com>
2022-10-09 19:12:17 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -28,14 +28,31 @@ Bot Owner Only
 """
 
 ABOUT = """
-➥ Name :  KMTZ Auto Filter V2335
-    
-➥ Creator : [ പോടാ പ*** മോനെ](
-https://t.me/kmtz_channel_v3)
 
-➥ Language : നിന്റെ തന്ത 
+𝗧𝗛𝗜𝗦 𝗔𝗡𝗡𝗔 𝗕𝗘𝗡'𝗦 𝗔𝗕𝗢𝗨𝗧 𝗠𝗦𝗚
 
-➥ Library : Pyrogram Asyncio 2 3 3 5
+✵✵✵✵✵✵✵✵✵✵✵✵✵✵✵✵✵✵✵✵
 
-➥ Tutorial : [Click Me](https://t.me/kmtz_v3)
-"""
+╔════❰ ꪖ᥇ꪮꪊ𝓽 ꪑ𝘴ᧁ ❱═❍⊱❁۪۪
+
+║╭━━━━━━━━━━━━━━━➣ 
+
+║┣⪼ 𝙈𝙔 𝙉𝘼𝙈𝙀 - <a href="https://t.me/Dqautofl_Bot"> 𝑨𝑵𝑵𝑨 𝑩𝑬𝑵 </a>
+
+║┣⪼ Ⓓ︎Ⓔ︎Ⓥ︎1 - <a href="https://t.me/pro_editor_tg"> Lalluᵗᵍ </a>
+
+║┣⪼ Ⓓ︎Ⓔ︎Ⓥ︎2 - <a href="https://t.me/PANDITHAN_SIR"> ⸙ꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋM-STER </a>
+
+║┣⪼ 𝓛𝓲𝓫𝓻𝓪𝓻𝓻𝔂 - 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
+
+║┣⪼ 𝓛𝓪𝓷𝓰𝓾𝓪𝓰𝓮 - 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
+
+║┣⪼ 𝓓𝓪𝓽𝓪 𝓑𝓪𝓼𝓮 - 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
+
+║┣⪼ 𝓑𝓸𝓽 𝓼𝓮𝓻𝓿𝓮𝓻 -  𝙷𝙴𝚁𝙾𝙺𝚄
+
+║┣⪼ 𝓑𝓾𝓲𝓵𝓭 𝓢𝓽𝓪𝓽𝓾𝓼 - v1.0.1 [ 𝙱𝙴𝚃𝙰 ]
+
+║╰━━━━━━━━━━━━━━━➣ ╚══════════════════❍⊱❁۪۪"""
+
+
